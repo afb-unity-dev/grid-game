2e028eec87b394b5087fa950bf66a29080eca179
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-19 22:14:20 +0300

    add grid content

---
+++ Assets/Scripts/Com/Afb/GridGame/Presentation/View/GridContent.cs
@@ -0,0 +1,69 @@
+using Com.Afb.GridGame.Presentation.View.Util.Extensions;
+using UnityEngine;
+using Zenject;
+
+namespace Com.Afb.GridGame.Presentation.View {
+    public class GridContent : MonoBehaviour {
+        // SerializeFields
+        [SerializeField, Range(0, 1)]
+        private float marginTop;
+        [SerializeField, Range(0, 1)]
+        private float marginBottom;
+        [SerializeField, Range(0, 1)]
+        private float marginLeft;
+        [SerializeField, Range(0, 1)]
+        private float marginRigth;
+
+        // Private Properties
+        private Bounds bounds;
+
+        // Public Properties
+        public Bounds Bounds => bounds;
+
+        private void Awake() {
+            SetBounds();
+        }
+
+        private void OnDrawGizmos() {
+            SetBounds();
+
+            float minX = bounds.min.x;
+            float maxX = bounds.max.x;
+            float minY = bounds.min.y;
+            float maxY = bounds.max.y;
+
+            Gizmos.DrawCube(bounds.center, new Vector3(0.5f, 0.5f, 0.5f));
+
+            Gizmos.DrawCube(new Vector3(minX, minY, 0), new Vector3(0.5f, 0.5f, 0.5f));
+            Gizmos.DrawCube(new Vector3(minX, maxY, 0), new Vector3(0.5f, 0.5f, 0.5f));
+            Gizmos.DrawCube(new Vector3(maxX, minY, 0), new Vector3(0.5f, 0.5f, 0.5f));
+            Gizmos.DrawCube(new Vector3(maxX, maxY, 0), new Vector3(0.5f, 0.5f, 0.5f));
+
+
+            Gizmos.DrawLine(new Vector3(minX, minY, 0), new Vector3(minX, maxY, 0));
+            Gizmos.DrawLine(new Vector3(minX, maxY, 0), new Vector3(maxX, maxY, 0));
+            Gizmos.DrawLine(new Vector3(maxX, maxY, 0), new Vector3(maxX, minY, 0));
+            Gizmos.DrawLine(new Vector3(maxX, minY, 0), new Vector3(minX, minY, 0));
+        }
+
+        private void SetBounds() {
+            Bounds cameraBounds = Camera.main.OrthographicBounds();
+
+            float camWidth = cameraBounds.max.x - cameraBounds.min.x;
+            float camHeight = cameraBounds.max.y - cameraBounds.min.y;
+            float top = camHeight * marginTop;
+            float bottom = camHeight * marginBottom;
+            float left = camWidth * marginLeft;
+            float right = camWidth * marginRigth;
+
+            float width = camWidth - left - right;
+            float height = camHeight - top - bottom;
+
+            float size = Mathf.Min(width, height);
+
+            var x = transform.position.x + left / 2 - right / 2;
+            var y = transform.position.y - top / 2 + bottom / 2;
+            bounds = new Bounds(new Vector3(x, y, 0), new Vector3(size, size, 0));
+        }
+    }
+}
