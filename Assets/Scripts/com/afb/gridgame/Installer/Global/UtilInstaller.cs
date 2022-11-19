8f46ce052dd8a0851b69bdc814ac449d765d8864
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-19 18:30:35 +0300

    add global installer

--- Assets/Scripts/com/afb/gridgame/Installer/Global/UtilInstaller.cs
+++ Assets/Scripts/com/afb/gridgame/Installer/Global/UtilInstaller.cs
@@ -4,6 +4,10 @@ using Zenject;
 namespace Com.Afb.GridGame.Installer.Global {
     public class UtilInstaller : MonoInstaller {
         public override void InstallBindings() {
+            BindResourceLoader();
+        }
+
+        private void BindResourceLoader() {
             Container.BindInterfacesTo<IResourceLoader>()
                 .FromNew()
                 .AsTransient()
