76e4e1303c3939baeeb0a0177394fdaabb94bca4
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-19 19:46:30 +0300

    add camera extensions

---
+++ Assets/Scripts/Com/Afb/GridGame/Presentation/View/Util/Extensions/CameraExtensions.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Com.Afb.GridGame.Presentation.View.Util.Extensions {
+    public static class CameraExtensions {
+        public static Bounds OrthographicBounds(this Camera camera) {
+            var t = camera.transform;
+            var x = t.position.x;
+            var y = t.position.y;
+            var size = camera.orthographicSize * 2;
+            var width = size * camera.aspect;
+            var height = size;
+
+            return new Bounds(new Vector3(x, y, 0), new Vector3(width, height, 0));
+        }
+    }
+}
