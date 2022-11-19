8ab61ceaee6b16cdc9980ba0bb21abbc2d510fcb
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-19 22:14:03 +0300

    add generic factory

---
+++ Assets/Scripts/Com/Afb/GridGame/Presentation/View/Util/Factory/GenericPrefabFactory.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Zenject;
+
+namespace Com.Afb.GridGame.Util.Factory {
+    public class GenericPrefabFactory<TValue> : IFactory<TValue>, IFactory<Transform, TValue> {
+        // Dependencies
+        [Inject]
+        private DiContainer container;
+
+        // Private Properties
+        private GameObject prefab;
+        private Transform parent;
+
+        public GenericPrefabFactory(GameObject prefab, Transform parent = null) {
+            this.prefab = prefab;
+            this.parent = parent;
+        }
+
+        public TValue Create() {
+            TValue view = container.InstantiatePrefabForComponent<TValue>(prefab, parent);
+            return view;
+        }
+
+        public TValue Create(Transform parent) {
+            TValue view = container.InstantiatePrefabForComponent<TValue>(prefab, parent);
+            return view;
+        }
+    }
+}
+
